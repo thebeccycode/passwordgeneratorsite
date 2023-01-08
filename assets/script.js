e21a0e7f35a217165a77518b3256646a6ad4e39e
Rebecca Sellers <rlsellers@hotmail.co.uk>
2023-01-08 15:44:23 +0000

    reverting file to original

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,31 @@ let generateLower = lowercase[Math.floor(Math.random() * lowercase.length)];
 
 let generateUpper = uppercase[Math.floor(Math.random() * lowercase.length)];
 
-//user choices with if else statements
+// Function to prompt user for password options
+function getPasswordOptions() {
 
-confirm("Would you like special characters?");
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+}
+
+// Function to generate password with user input
+function generatePassword() {
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);

69b57cee54fad8c83340ef6afdefd907cf35b265
Rebecca Sellers <rlsellers@hotmail.co.uk>
2023-01-08 15:25:02 +0000

    major changes

--- assets/script.js
+++ assets/script.js
@@ -1,131 +1,24 @@
-// Array of special characters to be included in password
-let specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
 
-// Array of numeric characters to be included in password
-let numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.']
 
-// Array of lowercase characters to be included in password
-let lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
+let numberCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-// Array of uppercase characters to be included in password
-let upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
+let lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-// Function for getting a random element from an array
-function getRandom(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-};
+let uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-// Function to generate password with user input
-function generateRandomNumber() {
-  return getRandom(numericCharacters);
-};
 
-function generateRandomLower() {
-  return getRandom(lowerCasedCharacters);
-};
+//functions  for retrieving a random element
 
-function generateRandomUpper() {
-  return getRandom(upperCasedCharacters);
-};
 
-function generateRandomSpecial() {
-  return getRandom(specialCharacters);
-};
+let generateSpecial = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
 
-//consolelog checks
-console.log(getRandom(numericCharacters));
-console.log(getRandom(lowerCasedCharacters));
-console.log(getRandom(upperCasedCharacters));
-console.log(getRandom(specialCharacters));
+let generateNumber = numberCharacters[Math.floor(Math.random() * numberCharacters.length)];
 
-let password = document.getElementById("password");
+let generateLower = lowercase[Math.floor(Math.random() * lowercase.length)];
 
-let generatedPassword = numericCharacters.concat(upperCasedCharacters + lowerCasedCharacters + specialCharacters );
+let generateUpper = uppercase[Math.floor(Math.random() * lowercase.length)];
 
-function generatePassword(){
-  let passwordLength = 10;
-  let password = ""; 
-  for (let i= 0; index < passwordLength; index++) {
-    let randomNumber = Math.floor(Math.random()* generatedPassword.length);
-    password += generatedPassword.substring(randomNumber, randomNumber +54);
-  };
-  document.getElementById("password").value = password;
-};
+//user choices with if else statements
+
+confirm("Would you like special characters?");

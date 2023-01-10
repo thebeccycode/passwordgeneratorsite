64f79650a231f5283cc90b0d4fc1366588e0e065
Rebecca Sellers <rlsellers@hotmail.co.uk>
2023-01-10 15:48:11 +0000

    reverting some changes

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,117 @@
+let specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
 
-let specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.']
+// Array of numeric characters to be included in password
+let numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-let numberCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+// Array of lowercase characters to be included in password
+let lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
 
-let lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
 
-let uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+// Function to prompt user for password options
+function getPasswordOptions() {
 
-confirm("do you want lowercase characters?");
-confirm("do you want uppercase characters?");
-confirm("do you want to use numbers?");
-confirm("do you want to use punctuation marks?");
+}
 
+// Function for getting a random element from an array
+function getRandom(arr) {
 
-//functions  for retrieving a random element
-function generateLower() {
-  return lowercase[Math.floor(Math.random() * lowercase.length)];
-};
-
-function generateUpper(){
-  return uppercase[Math.floor(Math.random()*uppercase.length)];
-};
-
-function generateNumber(){
-  return numberCharacters[Math.floor(Math.random() * numberCharacters.length)];
-};
-
-function generateSpecial(){
-  return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-};
-
-
-
+}
 
 // Function to generate password with user input
+function generatePassword() {
 
-
+}
 
 // Get references to the #generate element
-//var generateBtn = document.//querySelector('#generate');
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
-//function writePassword() {
- // let password = generatePassword();
- // let passwordText = document.//querySelector('#password');
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
 
-  //passwordText.value = password;
-//}
+  passwordText.value = password;
+}
 
 // Add event listener to generate button
-//generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener('click', writePassword);
